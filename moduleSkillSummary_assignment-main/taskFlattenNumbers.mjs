5a3ab0e8217bb3ddf21f967e0ca0271422a2182b
TobbiRL <torbjornrl@uia.no>
2025-02-20 14:46:33 +0100

    added number test to cube function, realized I should add to all func so did if n * 1 !== n return undefined, this is because when *1 with n you check if its a number, since any number *1 is the same as itself, if it is not, it wont == n and return undefined

--- moduleSkillSummary_assignment-main/taskFlattenNumbers.mjs
+++ moduleSkillSummary_assignment-main/taskFlattenNumbers.mjs
@@ -9,50 +9,71 @@ A function that returns the cube of a number
 A function that returns the area of a circle given the radius.
 A function that returns a greeting, given a name.
 */
-const tests = test("Testing function");
+const tests = test("Testing functions");
 
 function square(n) {
+    if (n * 1 !== n) {
+        return undefined
+    } else
     return n**2
 }
-
+console.log("Square tests")
 tests.isEqual(square(2), 4, "2 squared is 4") 
 tests.isEqual(square(3), 9, "3 squared is 9") 
 tests.isEqual(square(-3), 9, "-3 squared is 9") 
+tests.isEqual(square("test"), undefined, "Not a number") 
 
 function inchToMM(inchesGiven) {
+    if (inchesGiven * 1 !== inchesGiven) {
+        return undefined
+    } else
     return inchesGiven * 25.4
 }
-
+console.log("Inch to mm tests")
 tests.isEqual(inchToMM(1), 25.4, "1 inch is 25.4mm")
 tests.isEqual(inchToMM(2), 50.8, "2 inch is 50.8mm")
 tests.isEqual(inchToMM(-1), -25.4, "-1 inch is -25.4mm")
 tests.isEqual(inchToMM(1.5), 38.1, "1.5 inch is 38.1mm")
+tests.isEqual(inchToMM("test"), undefined, "Not a number")
 
 function root(n) {
+    if (n * 1 !== n) {
+        return undefined
+    } else
     return n**0.5;
 }
 
+console.log("Root tests")
 tests.isEqual(root(49), 7, "root of 49 is 7")
 tests.isEqual(root(81), 9, "root of 81 is 9")
 tests.isEqual(root(40), 6.324, "root of 40 is 6.324")
+tests.isEqual(root("test"), undefined, "Not a number")
 
 function cube(n) {
+    if (n * 1 !== n) {
+        return undefined
+    } else
     return n**3
 }
+
+console.log("Cube tests")
 
 tests.isEqual(cube(2), 8, "2 cubed is 8")
 tests.isEqual(cube(-2), -8, "-2 cubed is -8")
 tests.isEqual(cube(1.7), 4.913, "1.7 cubed is 4.913")
+tests.isEqual(cube("test"), undefined, "Not a number")
 
 function areaOfCircle(r) {
-    if (r <= 0) {
+    if (r <= 0 || r * 1 !== r) {
         return null
     } else
     return r**2*3.14
 
 }
 
+console.log("Area circle tests")
 tests.isEqual(areaOfCircle(1), 3.14, "area of a circle with radius 1 is 3.14")
 tests.isEqual(areaOfCircle(2), 12.56, "area of a circle with radius 2 is 12.56")
 tests.isEqual(areaOfCircle(321), 323548.74, "area of a circle with radius 321 is 323548.74")
 tests.isEqual(areaOfCircle(-1), null, "area of a circle with radius -1 is null")
+tests.isEqual(areaOfCircle("test"), null, "Not a number")
