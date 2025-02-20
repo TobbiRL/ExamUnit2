075152d20625468098f06d029c0b0e5e98787e3a
TobbiRL <torbjornrl@uia.no>
2025-02-20 13:10:12 +0100

    git commit test creating files and temporary task descriptions

---
+++ moduleSkillSummary_assignment-main/taskFunctionsPopping.mjs
@@ -0,0 +1,27 @@
+/* 
+In the file arrays.json there is an example of a an array that contains integers 
+and possibly other arrays. If you need more test data you can get as many as you 
+want from this source The task is to write a function that returns a flattened 
+version of any such array structure.
+
+NB! You might be tempted to use string manipulation, that is considered not 
+solving the task. This is a jagged array of integers, not a string.
+
+Example:
+
+[
+  435,
+  2028,
+  [
+    [
+      3047, 4910, 8146,
+      7999, 1433, 7211,
+      1197, 6002
+    ],
+    2821,
+    3508
+  ]
+]
+//--> 
+[435, 2028, 3047, 4910, 8146, 7999, 1433, 7211, 1197, 6002, 2821, 3508]
+*/
---
+++ moduleSkillSummary_assignment-main/taskMyBooks.mjs
@@ -0,0 +1,13 @@
+/*
+In the file books.json there is a listing of books. Write the functions to
+
+Return only books starting with The
+Return only books written by authors with a t in their name
+The number of books written after 1992
+The number of books written before 2004
+Return the isbn number of all the books for a given author.
+List books alphabetically assending or decendig
+List books chronologically assending or decendig
+List books grouped by author last name
+Lits books grouped by author first name
+*/
