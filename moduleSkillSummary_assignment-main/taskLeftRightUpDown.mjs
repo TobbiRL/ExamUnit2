fbc9dcee1ea6cd0a0ed1c0c2b8a1d2a41ef0fecf
TobbiRL <torbjornrl@uia.no>
2025-02-21 13:56:53 +0100

    spent a long time wondering what to do and imagining what to do understand I need a recursive depth and node, at lest i think so, to keep track after each {} in the example also know I want sum, nodes and deepest level returned, so that should be the values I track but am struggling to see the rest of what I need, imagining I need the code to enter each value set and read the data, but unsure how

--- moduleSkillSummary_assignment-main/taskLeftRightUpDown.mjs
+++ moduleSkillSummary_assignment-main/taskLeftRightUpDown.mjs
@@ -1,6 +1,6 @@
 /*
 
-In the file nodes.jsonthere is an example of a data structure that has a value of type integer, but also a left and right variable that could be a object of the same type. If you need more test data you can get as much as you want from this source
+In the file nodes.json there is an example of a data structure that has a value of type integer, but also a left and right variable that could be a object of the same type. If you need more test data you can get as much as you want from this source
 
 Calculate the sum of the full structure.
 Report the deepest level of the structure.
@@ -24,3 +24,34 @@ Deepest level = 4
 Nodes = 5
 
 */
+
+const testNode = {
+    value: 67, // Level 1
+    left: { value: 765, left: null, right: null }, // Level 2
+    right: {
+      value: 167, // Level 2
+      left: {
+        value: 564, // Level 3
+        left: null,
+        right: { value: 379, left: null, right: null } // Level 4
+      },
+      right: null
+    }
+  }
+
+function structureData(node, depth) {
+    if (node == null) {
+        return { sum: 0, deepestLevel: depth, nodes: 0}
+    }
+
+
+    
+
+
+
+    return {
+        sum,
+        deepestLevel,
+        Nodes,
+    }
+}
